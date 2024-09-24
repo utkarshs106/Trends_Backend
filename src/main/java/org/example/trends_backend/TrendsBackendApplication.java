00487267aab8b33b51b9746da8732712c4d28ab9
Utkarsh <43597586+utkarshs106@users.noreply.github.com>
2024-09-24 17:11:09 +0530

    Initial Commit

---
+++ src/main/java/org/example/trends_backend/TrendsBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.trends_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrendsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/trends_backend/TrendsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.trends_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
